dbbceec450970b7f7a415bd657cad629e625e4b5
likhithaG007 <likhithag007@gmail.com>
2026-09-20 11:53:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (57.17%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def addDigits(self, num):
+    
+        while num >= 10:
+
+            total = 0
+
+            while num > 0:
+                digit = num % 10
+                total = total + digit
+                num = num // 10
+
+            num = total
+
+        return num
